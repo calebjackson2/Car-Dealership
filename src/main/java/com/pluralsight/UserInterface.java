2dffcf04ae667d8e3d0192b58a9479d5f9b53e48
caleb jackson <jacksoncaleb740@gmail.com>
2025-10-29 09:31:49 -0400

    Car-Dealership

---
+++ src/main/java/com/pluralsight/UserInterface.java
@@ -0,0 +1,8 @@
+package com.pluralsight;
+
+public class UserInterface {
+    public void display() {
+        System.out.println("=== Dealership Application ===");
+        System.out.println("User Interface not yet implemented (Phase 2).");
+    }
+}
